e88200d8d35fb8998c654d6478ce87bbfa3652ee
captaim <mzcaptain@qq.com>
2016-08-21 21:18:15 +0800

    lua操作请求头--可以更改请求头内容

--- lua/request_header.lua
+++ lua/request_header.lua
@@ -1,11 +1,15 @@
-ngx.req.set_header('Foo','Bar1')
+ngx.req.set_header('Foo',{'Bar1','Bar2'})
+
+ngx.req.set_header('Foo1','Bar2')
+
+ngx.req.set_header('user-agent',{})
 
 local res = ngx.location.capture('/sub_request_header')
 
 if res.status == ngx.HTTP_OK then
 	ngx.say(res.body)
 else
-	ngx.exit(res.status)
+	ngx.exit(res.status) 
 end
 
 
