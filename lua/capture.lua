f23cf7b80b22be1f744db0cc6b4a76d81bd0944c
captaim <mzcaptain@qq.com>
2016-08-22 08:09:44 +0800

    父请求向子请求传递参数

--- lua/capture.lua
+++ lua/capture.lua
@@ -1,7 +1,11 @@
+ngx.req.read_body()
 
+local data = ngx.req.get_body_data()
 
-local res = ngx.location.capture('/sub_capture')
+local res = ngx.location.capture('/sub_capture',{body = data})
 
 ngx.say(res.body)
+
+--ngx.say(data)
 
 
