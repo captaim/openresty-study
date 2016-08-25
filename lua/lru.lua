9cf85c3f4d6749acdb56a6495caa9aee56f7b657
captaim <mzcaptain@qq.com>
2016-08-25 08:01:41 +0800

    当共享内存的长度达到上限的时候，后进去的值会剔除掉前面进去的值

--- lua/lru.lua
+++ lua/lru.lua
@@ -3,9 +3,13 @@ local shared = ngx.shared['ngx_cache']
 local suc,err,forc
 
 for index = 1 , 10000, 1 do
-	suc,err,forc = shared:set(tostring(index),'value' .. tostring(index))
+	suc,err,forc = shared:set(tostring(index),string.rep('a',1))
 end
 
-local keys = shared:get_keys()
+local keys = shared:get_keys(0)	--此处可以选择设置 『0』，选择读取最大的设置
 
-ngx.say(#keys)
+ngx.say(#keys)	--总长度为8095
+
+local value, flag = shared:get('1')	--此处我们会发现，当key=10000的时候，是存在值的，但是当key=1的时候，是没有值的，后进来的key(10000)会把之前的key剔除出去（因为总长度是8095）
+
+ngx.say(value)
